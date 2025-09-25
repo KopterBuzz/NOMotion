adbae05e3c6f710c18f4e35599e711fd7a15f5b6
Gabor Nemeth <etgabo@gmail.com>
2025-09-25 19:33:59 +0100

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace NOMotion
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+        private Aircraft aircraft;
+        private Vector3 rot;
+        private string rotString;
+        private UDPClient NOMotionUDP;
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            aircraft = null;
+            rot = Vector3.zero;
+            rotString = string.Empty;
+            NOMotionUDP = new UDPClient();
+
+        }
+
+        void Update()
+        {
+            GameManager.GetLocalAircraft(out aircraft);
+            if (null != aircraft)
+            {
+                rotString = $"{aircraft.rb.angularVelocity.z},{aircraft.rb.angularVelocity.x},{aircraft.rb.angularVelocity.y}";
+                NOMotionUDP.SendMotion(rotString);
+            }
+        }
+    }
+}
---
+++ UDPClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using UnityEngine;
+
+namespace NOMotion
+{
+    internal class UDPClient
+    {
+        UdpClient udp;
+        private int udpPort = 4123;
+        private string data;
+
+        public UDPClient()
+        {
+            udp = new UdpClient();
+            data = null;
+        }
+
+        public async void SendMotion(string data_)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(data_);
+            await udp?.SendAsync(bytes,bytes.Length,"127.0.0.1",udpPort);
+            Plugin.Logger?.LogDebug(data_);
+        }
+    }
+}
